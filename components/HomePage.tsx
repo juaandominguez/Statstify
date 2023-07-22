0a7e2cad55354425b585780d2fef547e652e1815
juaandominguez <juan.dominguezr@udc.es>
2023-07-23 00:06:17 +0200

    first commit

---
+++ components/HomePage.tsx
@@ -0,0 +1,47 @@
+"use client";
+import React from 'react'
+interface HomePageProps {
+    session: any
+}
+
+
+
+const HomePage: React.FC<HomePageProps> = ({session}) => {
+  const token = session.accessToken;
+  async function fetchWebApi(endpoint: string, method: string, body?: string) {
+    const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      method,
+      body:JSON.stringify(body)
+    });
+    return await res.json();
+  }
+
+async function getTopTracks(){
+  return (await fetchWebApi(
+    'v1/me/top/tracks?time_range=short_term&limit=5', 'GET'
+  )).items;
+}
+
+
+
+    async function handleCLick(){
+        const topTracks = await getTopTracks();
+          topTracks?.map((track: any, index: number) => (
+            console.log(`${index + 1}- ${track.name} Made by: `,
+            track.artists.map((artist: any) => (`${artist.name} `)))
+          )
+        )
+      }
+    return (
+    <>
+        <div className='flex h-[80vh] w-screen items-center justify-center'>
+          <div className='btn btn-primary' onClick={handleCLick}>FETCH DATA</div>
+        </div>
+    </>
+  )
+}
+
+export default HomePage
---
+++ components/Navbar.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import Image from 'next/image'
+import { signOut } from 'next-auth/react'
+
+interface NavbarProps {
+    session: any
+}
+
+const Navbar: React.FC<NavbarProps> = ({ session }) => {
+  const handleLogout = () => {
+    signOut({ callbackUrl: '/' })
+  }
+  return (
+    <>
+    <div className="navbar bg-base-100">
+        <div className="flex-1 ml-10">
+            <a className="btn btn-ghost normal-case text-xl">SpotiWrapped</a>
+        </div>
+        <div className="dropdown dropdown-end mr-10">
+      <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <Image 
+          src={session.user.image}
+          alt="Picture of the author"
+          width={40}
+          height={40}
+          />
+        </div>
+      </label>
+      <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+        <li>
+          <a className="justify-between">
+            Profile
+            <span className="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a onClick={handleLogout}>Logout</a></li>
+      </ul>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
