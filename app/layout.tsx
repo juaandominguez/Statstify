8dfdb76659733e82c50338e21922777d347cc049
juaandominguez <juan.dominguezr@udc.es>
2023-07-27 11:49:11 +0200

    fix SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,13 +18,34 @@ export default function RootLayout({
     <html lang="en" data-theme="forest">
       <head>
         <meta charSet="utf-8" />
-        <meta name="og:title" content="Statstify" />
+        <title>Statstify</title>
         <meta
-          name="og:description"
+          name="description"
           content="Check your spotify stats with Statstify"
         />
-        <meta name="og:url" content="https://statstify.vercel.app/" />
-        <meta name="og:image" content="https://statstify.vercel.app/og.png" />
+        <meta property="og:url" content="https://statstify.vercel.app" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Statstify" />
+        <meta
+          property="og:description"
+          content="Check your spotify stats with Statstify"
+        />
+        <meta
+          property="og:image"
+          content="https://statstify.vercel.app/og.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="statstify.vercel.app" />
+        <meta property="twitter:url" content="https://statstify.vercel.app" />
+        <meta name="twitter:title" content="Statstify" />
+        <meta
+          name="twitter:description"
+          content="Check your spotify stats with Statstify"
+        />
+        <meta
+          name="twitter:image"
+          content="https://statstify.vercel.app/og.png"
+        />
       </head>
       <body className={inter.className}>
         <NextAuthSessionProvider>{children}</NextAuthSessionProvider>
