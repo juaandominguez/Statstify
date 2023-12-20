583089d0f597676314da2be7f21875d616a93ba2
juaandominguez <juan.dominguezr@udc.es>
2023-12-20 20:53:04 +0100

    fix: redirection to album in recently played tracks

--- components/RecentlyPlayed.tsx
+++ components/RecentlyPlayed.tsx
@@ -99,7 +99,7 @@ const RecentlyPlayed: React.FC<RecentlyPlayedProps> = ({ recentTracks }) => {
                           ))}
                         </div>
                         <a
-                          href={item?.track?.album?.external_urls?.spotify}
+                          href={`${process.env.NEXT_PUBLIC_URL}/album/${item.track.album.id}`}
                           target="_blank"
                           className="ml-1 mr-3 line-clamp-1 cursor-pointer text-sm font-semibold duration-200 hover:text-white"
                           style={{
